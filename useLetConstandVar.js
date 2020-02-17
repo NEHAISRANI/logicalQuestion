1a0fc0a2508104e6646d5c64f8fe6a14340f287a
NEHAISRANI <nehai18@navgurukul.org>
2020-02-17 21:50:52 +0530

    first commit

---
+++ useLetConstandVar.js
@@ -0,0 +1,7 @@
+let name="neha";
+let flag=true;
+if (flag){
+	let name="israni";
+	console.log(name)
+}
+console.log(name)
---
+++ usePop_ShiftandUnshift_Push.js
@@ -0,0 +1,13 @@
+arr=[1,2,3,4,5,6,7,8,9,10]
+arr.pop()
+console.log(arr)
+
+
+arr.unshift(0)
+console.log(arr)
+
+arr.shift(0)
+console.log(arr)
+
+arr.push(10)
+console.log(arr)
